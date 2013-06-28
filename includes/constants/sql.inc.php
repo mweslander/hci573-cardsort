7495e67ac9e0095169483fcd6a56773af3811582
Michael Weslander <michael@weslander.com>
2013-06-28 11:15:39 -0500

    Added Constants

---
+++ includes/constants/sql.inc.php
@@ -0,0 +1,10 @@
+<?php
+// These are the database constants
+
+// I used constants  and defined the table_name elsewhere
+defined('DB_HOST')   ? null : define('DB_HOST', 'localhost');
+defined('DB_USER')   ? null : define('DB_USER', 'hci573usort');
+defined('DB_PASS')   ? null : define('DB_PASS', 'hci573usort');
+defined('DB_NAME')   ? null : define('DB_NAME', 'hci573usort');
+
+// PHP tag ommitted intentionally
