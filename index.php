5f31fea9522568e54a845c109294cfdb89b90939
agreazel <agreazel@eit-ann-e6430.exnet.iastate.edu>
2013-06-28 15:14:14 -0500

    Removed include code for CSS file.

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 /*
  * This is the homepage for usort
  */
-
-echo '<style type="text/css">';
-    include 'resources/styles/main.css';
-    echo '</style>';
         
 echo "<h1>USort</h1>";
 echo "<h2>A UX card sorting utility</h2>";
