7656a85f1d6d3fbafd7e4019125c7f556c845e14
agreazel <agreazel@eit-ann-e6430.exnet.iastate.edu>
2013-06-30 15:06:53 -0500

    Added testpage.php to experiment with CSS.

---
+++ testpage.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>test page</title>
+    <link rel="stylesheet" type="text/css" href="resources/styles/main.css" />   
+</head>
+<body>
+    <h1>Heading test</h1>
+    <h2>Subheading test</h2>
+</body>
+</html>
+
+
+
+
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
