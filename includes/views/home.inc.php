d41878e5585a24ba018cd0bf0634871c090298a2
agreazel <agreazel@eit-ann-e6430.exnet.iastate.edu>
2013-06-30 16:37:36 -0500

    Updated include files for home and login

--- includes/views/home.inc.php
+++ includes/views/home.inc.php
@@ -3,4 +3,18 @@
 /*
  * Home
  */
+?>
+
+<html>
+    <head>
+        <title>home</title>
+        <link rel="stylesheet" type="text/css" href="resources/styles/home.css" />
+    </head>
+    <body>
+        <div id="home_content">
+            <p>Home content goes here. Probably a couple paragraphs about
+                program and a link to view demo.</p>
+        </div>
+    </body>
+</html>
 
--- includes/views/login.inc.php
+++ includes/views/login.inc.php
@@ -1,7 +1,39 @@
-<?php
 
-/*
- * Login Page
- * 
- * @author Ann Greazel
- */
+
+
+<!--Login Page
+    *
+    *
+    @author Ann Greazel
+    *
+    *
+-->
+
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="resources/styles/login.css"/>
+    </head>
+    <title></title>
+    <body>
+        <form id="loginform">
+            <table>
+                <tr>
+                    <td>Username/Email:</td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="user"/></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                </tr>
+                <tr>
+                    <td><input type="password" name="pass"/></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" name="login" value="Login" /></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
