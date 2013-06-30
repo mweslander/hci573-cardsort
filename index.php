c1c9c4968add3d644f5b82512813be2e61b35b19
agreazel <agreazel@eit-ann-e6430.exnet.iastate.edu>
2013-06-30 16:14:31 -0500

    included home and login pages to the index page

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
  * This is the homepage for usort
  *
  *
+ echo "<body><h1>USort</h1></body>";
+ echo "<body><h2>A UX card sorting utility</h2></body>";
+ *
  */
-echo "<body><h1>USort</h1></body>";
-echo "<body><h2>A UX card sorting utility</h2></body>";
 
+?>
 
+<html>
+    <head>
+        <title>home</title>
+        <link rel="stylesheet" type="text/css" href="resources/styles/main.css" />
+    </head>
+</html>
+<?php
+    include 'includes/views/home.inc.php';
+    include 'includes/views/login.inc.php';
