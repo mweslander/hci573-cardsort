832a91fa1107c8d87e26a6f9ffa176a121557cce
Brett Young <brett.scott.young@gmail.com>
2013-07-03 14:31:36 -0700

    Added Demo, Register, Login form to Home view

--- includes/views/home.inc.php
+++ includes/views/home.inc.php
@@ -1,12 +1,45 @@
-<?php
+<!-- Header is already included --> 
+<header>
+    DEMO
+</header>
+<section>
+    <form id="demoform" name="demo" action="#"  method="post">
+        <p>
+            Name: DEMO<br />
+            Password: DEMO
+        </p>
+        <label for="log_name">Demo Name</label><input type="text" name="log_name"></input>
+        <label for="log_user_password">Demo Password</label><input type="password" name="log_user_password"></input>  
+        <button id="btnDemo">Start the Demo</button>
+    </form>    
+</section>
 
-/*
- * Home
- */
 
-?>
+<header>
+    USER REGISTER
+</header>
+<section>
+    <form id="registerForm" name="register" action="#"  method="post">
+        <label for="reg_user_name">User Name</label><input type="text" name="reg_user_name"></input>
+        <label for="reg_user_password">User Password</label><input type="password" name="reg_user_password"></input>
+        <label for="reg_user_first_name">First Name</label><input type="text" name="reg_user_first_name"></input>
+        <label for="reg_user_last_name">Last Name</label><input type="text" name="reg_user_last_name"></input>
+        <label for="reg_user_email">Email</label><input type="text" name="reg_user_email"></input>
+        <button id="btnRegister">Register</button>
+    </form>
+</section>
 
-<!-- Header is already included -->        
+<header>
+    LOGIN
+</header>
+<section>
+    <form id="loginform" name="login" action="#"  method="post">
+        <label for="log_name">User Name</label><input type="text" name="log_name"></input>
+        <label for="log_user_password">User Password</label><input type="password" name="log_user_password"></input>  
+        <button id="btnLogin">Login</button>
+    </form>
+</section>
+
 <div id="home_content">
     <p>Home content goes here. Probably a couple paragraphs about
         program and a link to view demo.</p>
