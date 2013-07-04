e24df6c0ceeb6e2b7b9e45412067509bd84d818b
agreazel <agreazel@eit-ann-e6430.exnet.iastate.edu>
2013-07-04 16:49:39 -0500

    added function for the drop down log in(s)

--- resources/scripts/register.js
+++ resources/scripts/register.js
@@ -198,3 +198,13 @@ $(document).ready(function()
     }
     
 });
+
+$(document).ready(function(){
+	$('#login-trigger').click(function(){
+		$(this).next('#login-content').slideToggle();
+		$(this).toggleClass('active');					
+		
+		if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+			else $(this).find('span').html('&#x25BC;')
+		})
+});
