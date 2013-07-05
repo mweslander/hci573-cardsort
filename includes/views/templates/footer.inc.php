8dc042dc8d6abc43b2a7ca2727256ef2824131d7
Brett Young <brett.scott.young@gmail.com>
2013-07-05 15:24:18 -0700

    Added simple footer for placement

--- includes/views/templates/footer.inc.php
+++ includes/views/templates/footer.inc.php
@@ -1,9 +1,13 @@
 <?php
-
 /*
  * This is the HTML footer for uSort
  */
 ?>
+        <footer>
+            Copyrighted by Brain Sweat.
+        </footer>
+
+
         </div><!-- Closes wrapper div that is opened in header.inc.php -->
     </body><!-- Closes body tag that is opened in header.inc.php -->
 </html><!-- Main HTML tags -->
