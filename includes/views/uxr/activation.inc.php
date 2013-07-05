3fc53e4087bbe1c1ef482fe9a9c94bee05c14b4e
Brett Young <brett.scott.young@gmail.com>
2013-07-05 15:25:38 -0700

    Turned off session checking for activation page. Work in progress

--- includes/views/uxr/activation.inc.php
+++ includes/views/uxr/activation.inc.php
@@ -1,23 +1,35 @@
 <?php
-
 //ONLY USERS THAT HAVE LOGGED IN
 //SUCCUSSFUL CAN TRY TO ACTIVATE THE ACCOUNT
 //isset($_SESSION['loggedin'])
-if (true) : ?>
+/// IN PROGRESS
+$loggedin = TRUE;
+
+//if(AuthSession::getSession('loggedin')== TRUE){
+//    $loggedin = True;
+//}
+if ($loggedin == TRUE) :
+    
+    ?>
 
 
 
     <header>
         Activate Your Account
     </header>
-    <section>
-        <form action="?url=login/activate" method="post">
-            <label for="activate_av_code">Enter Your Activation Code</label>
-            <input id="activate_av_code" name="activate_av_code" type="text"></input>
-            <button >Active</button>
-        </form>
+    <section id="activate">        
+            <div id="message_error"> <!-- This is used for displaying errors returned from AJAX -->
+                <div></div>
+            </div>
+            <form id='activationForm' action="?url=login/activate" method="post">
+                <label for="activate_av_code">Enter Your Activation Code</label>
+                <input id="activate_av_code" name="activate_av_code" type="text"></input>
+                <button >Active</button>
+            </form>
 
-    </section>
+        </section>
 
-<?php endif; ?>
+    <?php endif; ?>
+<?php 
 
+?>
