7e56988b6977431e1b2c28de0643fc46cdbaaa33
Brett Young <brett.scott.young@gmail.com>
2013-07-08 08:20:56 -0700

    Updated Test Subject View with structure

--- includes/views/ts/home.inc.php
+++ includes/views/ts/home.inc.php
@@ -1,7 +1,51 @@
+
+<section id="uxtsControl">
+    <header>
+    <h2>Control Box</h2>
+    </header>
+    <section class="ActiveStudies">
+    <?php  
+
+
+        // DUMP THE TEST SUBJECTS CARD SORTS STUDIES HERE
+    
+    
+    
+    
+    ?>
+    </section>
+</section>
+
+
+<?php
+// THIRD
+// We need our main content area that functions in conjunction with the sidebar
+?>
+<section id="uxrView">
+    <header>
+        <h2>Cardsort View</h2>
+    </header>
+    <section>
+         <?php  
+
+
+        // DUMP THE TEST SUBJECTS CARDS HERE 
+        // After they click on a study in the side bar.
+         
+         
+    
+         ?>
+        
+        
+        
+        
+        
+    </section>
+</section>
+
+<div class="clear"></div>
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+
+// FOURTH
+// We need a footer. This is already included from the views/templates/footer.inc.php
